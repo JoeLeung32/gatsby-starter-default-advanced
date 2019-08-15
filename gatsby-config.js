7f08dc36f245999c687c4757e2be851e37e5a91f
Joe Leung <joe.c.leung@accenture.com>
2019-08-15 16:39:55 +0800

    gatsby-config.js make it smart; .env.* update; html.jsx update;

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,50 +2,37 @@ require('dotenv').config(
   { path: `./.env.${process.env.NODE_ENV}` },
 );
 
-const envPathPrefix = process.env.ACN_PATH_PREFIX || '';
 const envGoogleAnalyticsTrackingID = process.env.GA_TRACKING_ID || '';
+const envPathPrefix = process.env.ACN_PATH_PREFIX || '';
+const envSiteMetadata = {
+  title: process.env.ACN_HEAD_META_TITLE || '',
+  description: process.env.ACN_HEAD_META_DESCRIPTION || '',
+  author: process.env.ACN_HEAD_META_AUTHOR || '',
+  googleAnalyticsTrackingId: envGoogleAnalyticsTrackingID,
+};
+const pluginList = [
+  'gatsby-plugin-react-helmet',
+  'gatsby-transformer-sharp',
+  'gatsby-plugin-sharp',
+  'gatsby-plugin-offline',
+  'gatsby-plugin-sass',
+  'gatsby-transformer-remark',
+];
 
 module.exports = {
   pathPrefix: envPathPrefix,
-  siteMetadata: {
-    title: 'Gatsby Default Starter',
-    description: 'Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.',
-    author: '@gatsbyjs',
-    googleAnalyticsTrackingId: envGoogleAnalyticsTrackingID,
-  },
+  siteMetadata: envSiteMetadata,
   plugins: [
-    'gatsby-plugin-react-helmet',
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-sharp',
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    'gatsby-plugin-offline',
-    'gatsby-plugin-sass',
-    'gatsby-transformer-remark',
+    ...pluginList,
     {
       resolve: 'gatsby-plugin-google-gtag',
       options: {
-        // You can add multiple tracking ids and a pageview event will be fired for all of them.
         trackingIds: [
-          envGoogleAnalyticsTrackingID, // Google Analytics / GA
-          // "AW-CONVERSION_ID", // Google Ads / Adwords / AW
-          // "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+          envGoogleAnalyticsTrackingID,
         ],
-        // This object gets passed directly to the gtag config command
-        // This config will be shared across all trackingIds
-        // gtagConfig: {
-        //   optimize_id: "OPT_CONTAINER_ID",
-        //   anonymize_ip: true,
-        //   cookie_expires: 0,
-        // },
-        // This object is used for configuration specific to this plugin
         pluginConfig: {
-          // Puts tracking script in the head instead of the body
           head: true,
-          // Setting this parameter is also optional
           respectDNT: true,
-          // Avoids sending pageview hits from custom paths
-          // exclude: ['/preview/**', '/do-not-track/me/too/'],
         },
       },
     },
@@ -58,7 +45,7 @@ module.exports = {
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'src/images/gatsby-icon.png',
       },
     },
     {
